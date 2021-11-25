c4d90f0acc63bc56cc2d2764d0331472d195514d
pyzak117 <86309102+pyzak117@users.noreply.github.com>
2021-11-25 11:43:08 +0100

    Update telenvi.py

--- telenvi.py
+++ telenvi.py
@@ -3,177 +3,155 @@
 """
 telenvi package
 ---------------
-Version = 0.0.1
-Oct. 2021
+Version = 1.0
+Nov. 2021
 """
 
 # Standard librairies
 import os
 import re
-from typing import List
+import sys
 
 # Third-Party librairies
 import numpy as np
-import gdal
-from matplotlib import pyplot as plt
+from osgeo import gdal
+from osgeo.gdalconst import *
 
-class Imsat:
+def load_bands(fPath, 
+               product = None, 
+               reflectance = False,
+               pattern = None,
+               endKeyPos = 1000,
+               cropZone = None):
 
-    """
-    description
-    -----------
-    Represent a satellite image
-    Each band is automatically convert in numpy.ndarray
-    attributes
-    ----------
-        blue(ndarray) : blue band
-        red(ndarray) : red band
-        ...
-    mandatory parameters
-    --------------------
-        imgPath(str): bands folder location
-    optionnals parameters
-    ---------------------
-        cropZone(list) : a list of tuples which represent top-left and bottom-right points of a region of interest
-        product(str) : specify the image platform (his sensor source)
-                       default value : "LS8"
-                       enables values : {"LS8", "S2A"}
-    """
+    # Known-products infos
+    product_data = {
+        "S2A":{
+            "pattern":"B[0-9]+_10M.JP2$",
+            "pos":-8
+            },
 
-    def __init__(self, imgPath, cropZone = None, product = "LS8"):
+        "LS8":{
+            "pattern":"B[0-9]+.TIF$",
+            "pos":-4
+            }
+    }
 
-        # Subregion to crop
-        CROP = False
+    # 1. Inputs checking
+    ####################
 
-        if type(cropZone) == list:
-            if len(cropZone) != 2:
-                raise ValueError("wrong coordinates format")
-
-            # Unpack crop coordinates
-            xMin, yMax = cropZone[0]
-            xMax, yMin = cropZone[1]
-
-            # Check coordinates logic validity
-            if xMin >= xMax or yMin >= yMax :
-                raise ValueError("Coordonnées de la zone d'intérêt invalides")
-
-            # Crop mode activated
-            CROP = True
-
-        print("Bands research and loading")
-        bands = {}
-
-        if product == "LS8":
-
-            for file in os.listdir(imgPath):
-
-                if file.upper().endswith(".TIF"):
-
-                    # Building pattern with regular expression
-                    motif = re.compile("B[0-9]+.TIF$") # [0-9]+ = 1 or many numbers
-                                                    # $ = string end
-
-                    try :
-                        # pattern position in filename
-                        pos = re.search(motif, file.upper()).span()[0]
-                    
-                    except AttributeError:
-                        continue # pattern not in filename : switch to next file
-
-                    bandId = file[pos:-4] # "imageNameB12.tif"[pos:-4] = "B12"
-                    fileBandName = os.path.join(imgPath, file)
-                    ds = gdal.Open(fileBandName)
-
-                    if CROP:
-                        # Get geographic data from the dataset
-                        geoTransform = ds.GetGeoTransform()
-
-                        # Unpack geotransform
-                        orX = geoTransform[0] # or > origine
-                        orY = geoTransform[3]
-                        widthPix = geoTransform[1]
-                        heightPix = geoTransform[5]
-                        
-                        # Zone framing
-                        row1=int((yMax-orY)/heightPix)
-                        col1=int((xMin-orX)/widthPix)
-                        row2=int((yMin-orY)/heightPix)
-                        col2=int((xMax-orX)/widthPix)
-
-                        # ndarray conversion
-                        band = ds.ReadAsArray(col1,row1,col2-col1+1,row2-row1+1).astype(np.float32)
-
-                    else:
-                        band = ds.ReadAsArray().astype(np.float32)
-
-                    # stock array in dic bands with bandId as key
-                    bands[bandId] = band
-
-                    print("{} loaded".format(bandId))
-
-            self.blue = bands["B2"]
-            self.green = bands["B3"]
-            self.red = bands["B4"]
-            self.nir = bands["B5"]
-            self.swir1 = bands["B6"]
-            self.swir2 = bands["B7"]
-            self.panchro = bands["B8"]
-            self.thermal1 = bands["B10"]
-
-        elif product == "S2A":
-
-            for fileName in os.listdir(imgPath):
-
-                # Building pattern with regular expression
-                motif = re.compile("B[0-9]+_10M.JP2$") # [0-9]+ = 1 or many numbers
-                                                    # $ = string end
-
-                try :
-                    # get pattern position in filename
-                    pos = re.search(motif, fileName.upper()).span()[0]
-                
-                except AttributeError:
-                    continue # pattern not in filename : switch to next fileName
-
-                bandId = fileName[pos:-8] # "imageNameB12.tif"[pos:-4] = "B12"
-                fileBandName = os.path.join(imgPath, fileName)
-                ds = gdal.Open(fileBandName)
-
-                if CROP:
-                    # Get geographic data from the dataset
-                    geoTransform = ds.GetGeoTransform()
-
-                    # Unpack geotransform
-                    orX = geoTransform[0] # or > origine
-                    orY = geoTransform[3]
-                    widthPix = geoTransform[1]
-                    heightPix = geoTransform[5]
-                    
-                    # Zone framing
-                    row1=int((yMax-orY)/heightPix)
-                    col1=int((xMin-orX)/widthPix)
-                    row2=int((yMin-orY)/heightPix)
-                    col2=int((xMax-orX)/widthPix)
-
-                    # Ndarray conversion
-                    band = ds.ReadAsArray(col1,row1,col2-col1+1,row2-row1+1).astype(np.float32)
-
-                else:
-                    band = ds.ReadAsArray().astype(np.float32)
-
-                # stock array in dic bands with bandId as key
-                bands[bandId] = band
-
-                print("{} loaded".format(bandId))
-
-            self.blue = bands["B02"]
-            self.green = bands["B03"]
-            self.red = bands["B04"]
-            self.nir = bands["B08"]
-
-    def show(self):
-        plt.imshow(self.blue) + plt.imshow(self.red) + plt.imshow(self.green)
-        plt.show()
+    # Classic mode
+    if product != None:
+        if product in product_data.keys():
+            pattern = product_data[product]["pattern"]
+            endKeyPos = product_data[product]["pos"]
+        else:
+            raise ValueError("error 1 : Unknown product {}".format(product))
     
-    def compoCol(self, bands):
-        pass
+    # Custom mode
+    else:
+        if pattern == None:
+            raise ValueError("error 2 : undefined pattern")
+
+        if type(pattern) != str:
+            raise ValueError("error 3 : pattern must be str")
+        
+        if type(endKeyPos) != int:
+            raise ValueError("error 5 : end key position must be integer")
+
+    CROP = False
+    if cropZone != None:
+        if type(cropZone) != list:
+            raise ValueError("error 6 : Coordinates of cropZone must be a list of 2 tuples")
+            
+        elif len(cropZone) != 2:
+            raise ValueError("error 6.1 : Coordinates of cropZone must be a list of 2 tuples")
+        
+        else:
+            if len(cropZone[0]) != 2 or len(cropZone[1]) != 2:
+                raise ValueError("error 7 : cropZone tuples must have 2 integers")
+
+        # Unpack crop coordinates
+        xMin, yMax = cropZone[0]
+        xMax, yMin = cropZone[1]
+
+        # Check coordinates logic validity
+        if xMin >= xMax or yMin >= yMax :
+            raise ValueError("error 8 : Coordinates are invalid")
+
+        # Crop mode activated
+        CROP = True
+
+    # Compile pattern with regular expression
+    rpattern = re.compile(pattern)
+
+    # 2. Loading data
+    ################
+
+    bands = {}
+    geoData = False
+
+    print("Bands research and loading")
+    for fileName in os.listdir(fPath):
+        try :
+            # get pattern start position in filename
+            startKeyPos = re.search(rpattern, fileName.upper()).span()[0]
+        
+        except AttributeError:
+            continue # pattern not in filename : switch to next file
+
+        fileBandName = os.path.join(fPath, fileName)
+        ds = gdal.Open(fileBandName)
+        bandId = fileName[startKeyPos:endKeyPos] # "imageNameB12.tif"[pos:-4] = "B12"
+
+        if not geoData:
+
+            # Get geographic data from the dataset
+            geoTransform = ds.GetGeoTransform()
+            projection = ds.GetProjection()
+
+            if CROP:
+                # Unpack geotransform
+                orX = geoTransform[0] # or > origine
+                orY = geoTransform[3]
+                widthPix = geoTransform[1]
+                heightPix = geoTransform[5]
+
+                # Zone framing
+                row1=int((yMax-orY)/heightPix)
+                col1=int((xMin-orX)/widthPix)
+                row2=int((yMin-orY)/heightPix)
+                col2=int((xMax-orX)/widthPix)
+                band = ds.ReadAsArray(col1,row1,col2-col1+1,row2-row1+1).astype(np.float32)
+
+            else:
+                band = ds.ReadAsArray().astype(np.float32)
+        if CROP:
+            crop_orX = xMin
+            crop_orY = yMax
+            nGeoTransform = (crop_orX, widthPix, 0.0, crop_orY, 0.0, heightPix)
+
+        # stock array in dic bands with bandId as key
+        if reflectance:
+            bands[bandId] = band/10000
+
+        else:
+            bands[bandId] = band
+        print("{} loaded".format(bandId))
+
+    if CROP:
+        return [bands, nGeoTransform, projection]
+
+    else:
+        return [bands, geoTransform, projection]
+
+def writeGeotiff(band, rows, cols, geoTransform, projection,outPath,driverName = "GTiff"):
+    
+    driver = gdal.GetDriverByName(driverName)
+    outData = driver.Create(outPath, cols, rows, 1, gdal.GDT_Int32)
+    outData.SetGeoTransform(geoTransform)
+    outData.SetProjection(projection)   
+    outData.GetRasterBand(1).WriteArray(band)
+    outData.GetRasterBand(1).SetNoDataValue(10000)
+    outData.FlushCache()
+    return None
