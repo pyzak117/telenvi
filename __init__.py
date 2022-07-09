5f1a5a453aeade4b460dd230a00acec44e9530ba
Thibaut Duvanel <thibaut.duvanel@univ-savoie.fr>
2022-07-09 12:34:18 +0200

    3.1.0

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 """
 telenvi
 -------
-Version = '3.0.9'
-2022-07-06 12:43:28
+Version = '3.1.0'
+2022-07-09 12:29:49
 """
 
-VERSION = '3.0.9'
+VERSION = '3.1.0'
 print("---------\ntelenvi package " + str(VERSION) + "\n---------")
